eec8c881fda30503404eb618f02c45a3d062e6bc
Hasnain <hasnain5f7@gmail.com>
2026-02-07 23:03:27 +0500

    fix: declare global prisma variable to ensure proper client initialization

--- src/lib/prisma.tsx
+++ src/lib/prisma.tsx
@@ -1,6 +1,10 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
 export const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
 });
