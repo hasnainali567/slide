cce8126e727819a467e7f18b6583b19e0f843348
Hasnain <hasnain5f7@gmail.com>
2026-02-07 22:48:31 +0500

    feat: update Automation model to allow optional name with default value and enhance createAutomations function for better error handling and retrieval of all automations

--- src/actions/automations/index.ts
+++ src/actions/automations/index.ts
@@ -1,20 +1,39 @@
 "use server";
 
 import { currentUser } from "@clerk/nextjs/server";
-import { createAutomation } from "./query";
+import { createAutomation, getAutomations } from "./query";
 
-export const getAllAutomations = async () => {
+export const createAutomations = async (id? : string) => {
   const user = await currentUser();
   try {
     if (!user) {
-      return { status: 401, data: "Unauthorized" };
+      return { status: 401, message: "Unauthorized" };
     }
-    const create = await createAutomation(user.id);
+    const create = await createAutomation(user.id, id);
     if (create) {
-      return { status: 201, data: "Automation created" };
+      return { status: 200, message: "Automation created" };
     }
-    return { status: 500, data: "Failed to create automation" };
+    return { status: 500, message: "Failed to create automation" };
   } catch (error) {
-    return { status: 500, data: "Internal Server Error" };
+    console.log(error);
+    
+    return { status: 500, message: "Internal Server Error from createAutomation" };
   }
 };
+
+
+export const getAllAutomations = async () => {
+    const user = await currentUser();
+    try {
+        if (!user) {
+            return { status: 401, data: "Unauthorized" };
+        }
+        const automations = await getAutomations(user.id)
+        if(automations) {
+            return { status: 200, data: automations.automations }
+        }
+        return { status: 404, data: "No automations found" }
+    } catch (error) {
+        return { status: 500, data: "Internal Server Error" };
+    }
+}
