c38172e1552190db875f5cd686bc1b1dbe109d01
Hasnain <hasnain5f7@gmail.com>
2026-02-07 18:19:46 +0500

    fix: correct user authorization handling in getAllAutomations function

--- src/actions/automations/index.ts
+++ src/actions/automations/index.ts
@@ -1,18 +1,20 @@
-'use server'
+"use server";
 
 import { currentUser } from "@clerk/nextjs/server";
 import { createAutomation } from "./query";
 
 export const getAllAutomations = async () => {
-    const user = await currentUser();
-    try {
-        const create = await createAutomation(user.id)
-        if(create) {
-            return {status : 201, data : 'Automation created'}
-
-        }
-        return {status : 500, data : 'Failed to create automation'}
-    } catch (error) {
-        return {status : 500, data : 'Internal Server Error'}
+  const user = await currentUser();
+  try {
+    if (!user) {
+      return { status: 401, data: "Unauthorized" };
     }
-}
+    const create = await createAutomation(user.id);
+    if (create) {
+      return { status: 201, data: "Automation created" };
+    }
+    return { status: 500, data: "Failed to create automation" };
+  } catch (error) {
+    return { status: 500, data: "Internal Server Error" };
+  }
+};
