68cfb14a7d61096fca3104a6bc5123855af48d6a
Hasnain <hasnain5f7@gmail.com>
2026-02-08 11:13:51 +0500

    fix: remove unused props type declaration in Page component

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,13 +1,12 @@
 import { onBoard } from '@/actions/user'
 import { redirect } from 'next/navigation'
 
-type Props = {}
 
-const Page = async (props: Props) => {
+const Page = async () => {
 
   const user = await onBoard()
   if(user?.status === 200 || user?.status ===  201) { 
-    return redirect(`/dashboard/${user.data?.firstname.toLocaleLowerCase()}${user.data?.lastname.toLocaleLowerCase()}`)
+    return redirect(`/dashboard/${user.data?.firstname?.toLocaleLowerCase()}${user.data?.lastname?.toLocaleLowerCase()}`)
   }
 return redirect('/sign-in')
 }
