cbedd5a64a9ec5fe2e6e53eb14bdb3a16abcbd4f
Hasnain <hasnain5f7@gmail.com>
2026-02-12 07:47:29 +0500

    feat: integrate Google GenAI for content generation in dashboard
    - Added @google/genai dependency to package.json.
    - Implemented AI content generation in the dashboard page, allowing users to generate a short story about a robot learning to love.
    - Created a new gemini.ts file to initialize the GoogleGenAI instance with the API key.

--- src/app/(protected)/dashboard/[slug]/page.tsx
+++ src/app/(protected)/dashboard/[slug]/page.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+'use client'
+import ai from '@/lib/gemini'
 
 type Props = {}
 
 const Page = (props: Props) => {
+  const generateResponse = async() => {
+    const response = await ai.models.generateContent({
+      model: 'gemini-1.5-pro',
+      contents: {
+        text: 'Write a short story about a robot learning to love.'
+      },
+    })
+    console.log(response);
+  }
   return (
-    <div>Page</div>
+    <div><button onClick={generateResponse}>Generate</button></div>
   )
 }
 
---
+++ src/lib/gemini.ts
@@ -0,0 +1,7 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({
+    apiKey : "AIzaSyAJRsVnqgsWcmiZ5zwl808KD_u5aBriOwg",
+})
+
+export default ai;
